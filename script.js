f4bb7acbe77374bfecf3db52e45693345f3cb932
Balqees <oladipupobilikisarike@gmail.com>
2026-02-27 13:23:28 +0100

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const nextBtns = document.querySelectorAll(".next-btn");
 const steps = document.querySelectorAll(".form-step");
 const sidebarSteps = document.querySelectorAll(".step");
-const prevBtns = document.querySelectorAll(".prev-btn")
+const prevBtns = document.querySelectorAll(".prev-btn");
+const projectCards = document.querySelectorAll(".project-card");
 
 let currentStep = 0;
+let selectedProject ='';
 
 function updateSteps() {
   steps.forEach((step, index) => {
@@ -19,6 +21,13 @@ function updateSteps() {
 
 nextBtns.forEach(btn => {
   btn.addEventListener("click", () => {
+
+    // If on Project Step (index 1) and nothing selected
+    if (currentStep === 1 && !selectedProject) {
+      alert("Please select a project type.");
+      return;
+    }
+
     if (currentStep < steps.length - 1) {
       currentStep++;
       updateSteps();
@@ -34,3 +43,22 @@ prevBtns.forEach(btn => {
     }
   });
 });
+
+projectCards.forEach(card => {
+  card.addEventListener("click", () => {
+
+    projectCards.forEach(c => c.classList.remove("active"));
+
+    card.classList.add("active");   // FIXED
+
+    selectedProject = card.dataset.value;
+
+    console.log("Selected Project:", selectedProject);
+  });
+});
+// nextBtns.addEventListener("click", () => {
+//   if (!selectedProject) {
+//     alert("Please select a project type.");
+//     return;
+//   }
+// });
